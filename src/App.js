d795909d74c8f5716b70f2e500d9b5b6f85bb67d
Amirreza Mahmoudi <amirrezamh03@gmail.com>
2023-10-20 18:58:14 +0330

    nav2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import {BrowserRouter as Router} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar';
+import Home from './components/page/Home';
 
 function App() {
   return (
     <>
     <Router>
       <Navbar/>
+      <Routes>
+        <Route path="/" exact Component={Home}></Route>
+      </Routes>
     </Router>
     </>
   );
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,42 @@
-import React from "react";
+import React ,{useState} from "react";
+import { Link } from "react-router-dom";
 import './Navbar.css'
 function Navbar(){
+    const [menuState , setMenuState] = useState(false)
+    const menuHandler = () => setMenuState(!menuState)
+    const mobileHandler = () => setMenuState(false)
     return(
         <nav className="navbar">
-
+            <div className="navbar-container">
+                <Link to="/" className="navbar-logo">
+                    Travel <i className="fab fa-typo3"></i>
+                </Link>
+                <div className="menu-icon" onClick={menuHandler}>
+                    <i className={menuState ? 'fas fa-times' : 'fas fa-bars'}></i>
+                </div>
+            </div>
+            <ul className={menuState ? 'nav-menu active' : 'nav-menu' }>
+                <li className="nav-item">
+                    <Link to="/" className="nav-links" onClick={mobileHandler}>
+                        Home
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/" className="nav-links" onClick={mobileHandler}>
+                        Toursim
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/" className="nav-links" onClick={mobileHandler}>
+                        Tour
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/" className="nav-links-mobile" onClick={mobileHandler}>
+                        Sign Up
+                    </Link>
+                </li>
+            </ul>
 
         </nav>
 
